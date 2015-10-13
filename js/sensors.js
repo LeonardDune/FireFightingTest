ffeae0d48414b0e41cd2bb093e503adab519eca3
LeonardDune <rl.wouters@gmail.com>
2015-10-13 22:17:41 +0200

    Drawing over different sensors in different SVG's

--- js/sensors.js
+++ js/sensors.js
@@ -1,3 +1,4 @@
+
 d3.json("http://firefighterhubtest.mybluemix.net/tempsensors", function(data) {
 	var sensorData = data.rows;
 	var nestedSensorData = d3.nest()
@@ -6,25 +7,34 @@ d3.json("http://firefighterhubtest.mybluemix.net/tempsensors", function(data) {
 
 	nestedSensorData.forEach(function (el) {
 		el.numMeasures = el.values.length;
+
+		//console.log(el)
+
+		//var maxTemp = d3.max(el, function(el) {return el.values.value[0];});
+
+		var xScale = d3.scale.linear().domain([0,el.numMeasures]).range([0,500]);
+		var yScale = d3.scale.linear().domain([0,25]).range([0,100]);
+		var pix = 500/el.numMeasures;
+
+
+		d3.select("body")
+		.append("svg")
+		.attr("width", "500")
+		.attr("height", "100")
+		.selectAll("rect")
+		.data(el)
+		.enter()
+		.append("rect")
+		.attr("width",  " + pix + ")
+		.attr("height", " + pix + ")
+		.attr("x", function(d, i) {return i*xScale(d.numMeasures);})
+		.attr("y", function(d, i) {return 100 - yScale(d.values[i].value[0]);})
+		.style("fill", "blue");
 	})
 
-	var maxMeasures = d3.max(nestedSensorData, function(el) {return el.numMeasures;});
 
-	var yScale = d3.scale.linear().domain([0,maxMeasures]).range([0,100]);
 
 	//console.log(nestedSensorData);
 
-	d3.select("body")
-		.append("svg")
-		.selectAll("rect")
-		.data(nestedSensorData)
-		.enter()
-		.append("rect")
-		.attr("width","50")
-		.attr("height", function(d) {return yScale(d.numMeasures);})
-		.attr("x", function(d, i) {return i*60;})
-		.attr("y", function(d) {return 100 - yScale(d.numMeasures);})
-		.style("fill", "blue")
-		.style("stroke", "red")
-		.style("stroke-width", "1px").style("opacity", .25);
+
 });
