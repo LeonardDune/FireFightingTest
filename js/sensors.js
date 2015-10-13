88c3e4c2152b1b3525ef8d6203a9e7c60af609bd
LeonardDune <rl.wouters@gmail.com>
2015-10-13 21:39:40 +0200

    Show sensor data

--- js/sensors.js
+++ js/sensors.js
@@ -1,5 +1,30 @@
 d3.json("http://firefighterhubtest.mybluemix.net/tempsensors", function(data) {
-	var nestedSensors = d3.nest()
+	var sensorData = data.rows;
+	var nestedSensorData = d3.nest()
 		.key(function(el) {return el.key})
-		.entries(data);
+		.entries(sensorData);
+
+	nestedSensorData.forEach(function (el) {
+		el.numMeasures = el.values.length;
+	})
+
+	var maxMeasures = d3.max(nestedSensorData, function(el) {return el.numMeasures;});
+
+	var yScale = d3.scale.linear().domain([0,maxMeasures]).range([0,100]);
+
+	//console.log(nestedSensorData);
+
+	d3.select("body")
+		.append("svg")
+		.selectAll("rect")
+		.data(nestedSensorData)
+		.enter()
+		.append("rect")
+		.attr("width","50")
+		.attr("height", function(d) {return yScale(d.numMeasures);})
+		.attr("x", function(d, i) {return i*60;})
+		.attr("y", function(d) {return 100 - yScale(d.numMeasures);})
+		.style("fill", "blue")
+		.style("stroke", "red")
+		.style("stroke-width", "1px").style("opacity", .25);
 });
